31ccaa941424880ec472d274a1a071c75b8117c2
ayomide1636 <adepojumojirade920@gmail.com>
2025-05-26 09:21:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: '‎ayomide',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Professor', // ayomide
+  ownerNumber:  '+2349016368847'
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "public"/private,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.1",
 };
